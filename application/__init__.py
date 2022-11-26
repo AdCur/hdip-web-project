3feb882ba51b2633dd154621bedb62132b9aca11
AdCur <adkenzo@gmail.com>
2022-11-26 19:04:52 +0000

    db configuration

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,13 @@
-
+import requests
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crypto.db'
 app.config['SECRET_KEY'] = 'thisisasecret'
-
+db = SQLAlchemy(app)
+app.app_context().push()
+    
 from application import routes
+from application import models
