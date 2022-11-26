a330fe227bf8ee33e6b7dc70d6904e3c320ebdfc
AdCur <adkenzo@gmail.com>
2022-11-26 19:01:20 +0000

    functionality added

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,14 @@
-from application import app
-from flask import render_template
+from application import app, db
+from flask import render_template, request, redirect, url_for, flash
+from application.models import Crypto
+from application.forms import CryptoForm
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = CryptoForm()
+    if form.validate_on_submit():
+        new_crypto = Crypto(name = form.name.data)
+        db.session.add(new_crypto)
+        db.session.commit()
+    return render_template('index.html', form=form)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
