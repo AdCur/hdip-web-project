f7890f34f647ba205f2e460d4ecd095b9555be26
AdCur <adkenzo@gmail.com>
2022-12-01 17:43:10 +0000

    added bmi database

--- application/models.py
+++ application/models.py
@@ -5,5 +5,11 @@ class Crypto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+class Bmi(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.Float(), nullable=False) # centimetres
+    weight = db.Column(db.Float(), nullable=False) # pounds
+    bmi_result = db.Column(db.Float(),nullable=True) #bmi points
+
 # create database
 db.create_all()
